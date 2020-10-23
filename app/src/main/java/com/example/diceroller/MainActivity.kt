f48c821998b35be1fa836102ea95092390b80b99
sasiman02 <gustndlfrl@gmail.com>
2020-10-23 17:04:23 +0900

    좋은 코딩 습관 적용

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,24 +10,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rollDice()
 
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
     private fun rollDice() {
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            val dice = Dice(6)
-            val diceRoll = dice.roll();
-            val diceIamge: ImageView = findViewById(R.id.imageView)
-            when(diceRoll) {
-                1 -> diceIamge.setImageResource(R.drawable.dice_1)
-                2 -> diceIamge.setImageResource(R.drawable.dice_2)
-                3 -> diceIamge.setImageResource(R.drawable.dice_3)
-                4 -> diceIamge.setImageResource(R.drawable.dice_4)
-                5 -> diceIamge.setImageResource(R.drawable.dice_5)
-                6 -> diceIamge.setImageResource(R.drawable.dice_6)
-            }
+        val dice = Dice(6)
+        val diceRoll = dice.roll();
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
